b080df9e13fb4da8d0936177a3ce6fa88177883a
Firoz Ebna Jobaier <firoz.jobaier@gmail.com>
2022-11-02 10:04:35 +0600

    Initial commit

---
+++ src/main/java/bd/gov/barcapps/cms/CropZoningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package bd.gov.barcapps.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropZoningManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropZoningManagementSystemApplication.class, args);
+	}
+
+}
